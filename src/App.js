a0dd8779ab65cb4102ed4f04edaa512302c6dcde
bhushan354 <deshmukhbhushan354@gmail.com>
2023-09-15 06:29:06 +0530

    components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
-    <div className="App">
-      Hello the app is working fine and showing on as expected with only one error but its
-      only about an imge so no worries
-    </div>
+    <Calculator />
   );
 }
 

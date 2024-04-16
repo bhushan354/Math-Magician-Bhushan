68aabb0eab81a1ef6574f0411968a848b9163341
bhushan354 <deshmukhumesh354@gmail.com>
2024-04-16 12:32:03 +0530

    Add progressbar

--- src/api/fetchQuote.js
+++ src/api/fetchQuote.js
@@ -1,35 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import '../stylesheet/progressbar.css'; import React, { useState, useEffect } from 'react';
 
 function FetchQuote() {
   const [data, setData] = useState(null);
   const [error, setError] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [progress, setProgress] = useState(0);
 
   const fetchData = async () => {
     setIsLoading(true);
+    setProgress(20);
     try {
-      const response = await fetch(
-        'https://api.api-ninjas.com/v1/quotes?category=success',
-        {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            'X-Api-Key': 'h51HAduSmK3j8nDhJsGZ7g==UOrxjLtOdw50QXKB',
-          },
+      const response = await fetch('https://api.api-ninjas.com/v1/quotes?category=success', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Api-Key': 'h51HAduSmK3j8nDhJsGZ7g==UOrxjLtOdw50QXKB',
         },
-      );
+      });
+
+      setTimeout(() => setProgress(50), 500);
+
       const responseData = await response.json();
       if (response.ok) {
         setIsLoading(false);
         setError(false);
+        setProgress(100);
         setData(responseData[0]);
       } else {
         setIsLoading(false);
-        setError([responseData]);
+        setProgress(100);
+        setError(responseData);
       }
     } catch (error) {
       setIsLoading(false);
       setError(true);
+      setProgress(100);
     }
   };
 
@@ -37,7 +42,15 @@ function FetchQuote() {
     fetchData();
   }, []);
 
-  if (isLoading) return <div className="quote-container">Loading...</div>;
+  if (isLoading) {
+    return (
+      <div className="quote-container">
+        <div className="progress-bar-container">
+          <div className="progress-bar" style={{ width: `${progress}%` }} />
+        </div>
+      </div>
+    );
+  }
 
   if (error) return <div className="quote-container">Something went wrong!</div>;
 
